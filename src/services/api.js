cae7c284b398c7a4664b5ab807a480cf7cffb1f5
ilionroot <ilion.root@gmail.com>
2022-04-06 06:34:45 -0300

    Fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://vps38322.publiccloud.com.br:3030",
+  baseURL: "http://127.0.0.1:3030",
   // baseURL: "http://192.168.100.8:3030/",
 });
 
