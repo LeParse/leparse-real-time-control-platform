8529ac3cdd7ace6a157e5b973f30bb5944b55780
ilionroot <ilion.root@gmail.com>
2022-04-29 18:16:27 -0300

    Relatorios Fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://vps38322.publiccloud.com.br:3030",
-  baseURL: "http://localhost:3030/",
+  baseURL: "http://vps38322.publiccloud.com.br:3030",
+  // baseURL: "http://localhost:3030/",
 });
 
 api.interceptors.response.use(
