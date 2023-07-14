eeaee93caa0b3a2970d4cdc22626b1b7407a2982
Igor Augusto Gomes de Melo <ilionroot@MacBook-Air-de-Igor.local>
2023-07-14 11:35:33 -0300

    Last try

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.leparse.tech:3030",
+  baseURL: "https://api.leparse.tech/real-time",
   // baseURL: "http://localhost:3030/",
 });
 
